ea247cfc84431330f11132ac7a09405ec608f4ef
jp222wh <jeppssonman@gmail.com>
2016-09-17 14:49:48 +0200

    Squashed 'data-type-determinator/' content from commit ef28690
    git-subtree-dir: data-type-determinator
    git-subtree-split: ef28690e0b3dfb50ec281de611c9cdf967b0436f

---
+++ src/determinator.js
@@ -0,0 +1,24 @@
+/**
+ * Module for determinator.
+ *
+ * @author John Häggerud
+ * @author Mast Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Returns a string indicating the type of the data sent to the function.
+ *
+ * @param {*} data The data whose type is to be returned.
+ * @returns {String} The data type of the parameter.
+ */
+exports.tellType = function(data) {
+
+  let result = 'You sent me ';
+
+  // TODO: Write your code here.
+
+  return result;
+};
