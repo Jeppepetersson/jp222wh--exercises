81efb3cb9e273c11e70cb35c7cd75eb6dbdd8a7b
jp222wh <jeppssonman@gmail.com>
2016-09-18 08:53:44 +0200

    Squashed 'additional-array-copy/' content from commit 7a60fba
    git-subtree-dir: additional-array-copy
    git-subtree-split: 7a60fba1e622fd8b7077dc2ddfddbbc32d2e46f7

---
+++ src/my-array.js
@@ -0,0 +1,24 @@
+/**
+ * Module for pushToArray.
+ *
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Returns a copy of an array where a number has been added to the end of the copy.
+ *
+ * @param {Array} source The array to create a copy of.
+ * @param {Number} number The number to add to the end of the copy of the array.
+ * @throws {TypeError} The source parameter must be an Array; number parameter must be a Number.
+ * @throws {Error} The source parameter must not be an empty array.
+ * @returns {Array} A copy of the source array with an additional number.
+ */
+module.exports.pushToArray = function(source, number) {
+
+  // TODO: Write your code here.
+
+};
