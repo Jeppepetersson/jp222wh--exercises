d6d6c21a46796216009da10d82eac6bdc97b4a0f
jp222wh <jeppssonman@gmail.com>
2016-09-19 08:34:30 +0200

    Squashed 'better-than-avarage/' content from commit af4841b
    git-subtree-dir: better-than-avarage
    git-subtree-split: af4841b70582f2e44a4f598bebb93294bee77417

---
+++ src/betterThanAverage.js
@@ -0,0 +1,25 @@
+/**
+ * Module for betterThanAverage.
+ *
+ * @namespace bta
+ * @author John Häggerud
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Returns the name of the students whose points is greater than,
+ * or equal to, the average of the points.
+
+ * @param {Array<object>} students
+ * @returns {Array<string>} *
+ */
+let betterThanAverage = function(students) {
+
+  // TODO: Write your code here.
+
+};
+
+module.exports = betterThanAverage;
