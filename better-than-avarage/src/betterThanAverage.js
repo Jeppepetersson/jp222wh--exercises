adb1610c41e84af9ef87ddc3089a8bce3df149ff
jp222wh <jeppssonman@gmail.com>
2016-09-19 12:10:31 +0200

    understandeble, but there must be an easier way to calculate and sort calculated...

--- better-than-avarage/src/betterThanAverage.js
+++ better-than-avarage/src/betterThanAverage.js
@@ -18,8 +18,22 @@
  */
 let betterThanAverage = function(students) {
 
-  // TODO: Write your code here.
+  if (!Array.isArray(students)) {
+    throw new TypeError('students does not refer to an array');
+  }
 
+  if (students.length === 0) {
+    throw new Error('The Array can\'t be empty');
+  }
+  let average = students.reduce(function(current, next) {
+      return current + next.points;
+    }, 0) / students.length;
+
+  return students.filter(function(current) {
+    return current.points >= average;
+  }).map(function(current) {
+    return current.name;
+  });
 };
 
 module.exports = betterThanAverage;
