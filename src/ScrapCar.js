516168f332113b1f516cfaf7fe6921dae2b27586
jp222wh <jeppssonman@gmail.com>
2016-10-03 20:04:10 +0200

    Squashed 'scrap-car/' content from commit 382f6ae
    git-subtree-dir: scrap-car
    git-subtree-split: 382f6ae57e4915f76e64d756a29619d0097fc423

---
+++ src/ScrapCar.js
@@ -0,0 +1,15 @@
+/**
+ * Module for ScrapCar.
+ *
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+// TODO: Write your code here!
+
+/**
+ *  Exports.
+ */
+module.exports = ScrapCar;
