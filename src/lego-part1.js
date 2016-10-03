ad82871b80affd599e1448da3a2d7956ad6dc13e
jp222wh <jeppssonman@gmail.com>
2016-10-03 09:37:34 +0200

    Squashed 'lego-constructor/' content from commit 16b9010
    git-subtree-dir: lego-constructor
    git-subtree-split: 16b90108f650d940812bee3b61f2dd4d932ad74c

---
+++ src/lego-part1.js
@@ -0,0 +1,35 @@
+/**
+ * Module for lego part 1.
+ *
+ * @author Johan Leitet
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Returns an object representing a lego brick.
+ *
+ * @param {Number} [x = 2] - The horizontal number of knobs of the brick.
+ * @param {Number} [y = 4] - The vertical number of knobs of the brick.
+ * @param {String} [color = 'red'] - Color of the brick ('blue', 'red', 'green', etc.)
+ * @returns {Object} obj - Object representing a brick.
+ * @returns {Number} obj.x - Horizontal number of horizontal nobs of the brick.
+ * @returns {Number} obj.y - Vertical number of vertical nobs of the brick.
+ * @returns {String} obj.color - Color of the brick
+ * @returns {Function} obj.toString - A function returning a string representing the object.
+ * @returns {Function} obj.render - A function rendering the object.
+ */
+let createLegoBrick = function(x, y, color) {
+
+  // TODO: Write your code here.
+
+};
+
+/**
+ *  Exports.
+ *
+ * @type {createLegoBrick}
+ */
+exports.createLegoBrick = createLegoBrick;
---
+++ src/lego-part3.js
@@ -0,0 +1,31 @@
+/**
+ * Module for lego part 3.
+ *
+ * @author Johan Leitet
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+/**
+ * Creates a LegoBrick instance that represents a lego brick.
+ *
+ * @constructor
+ * @param {Number} [x = 2] - The horizontal number of knobs of the brick.
+ * @param {Number} [y = 4] - The vertical number of knobs of the brick.
+ * @param {String} [color = 'red'] - The color of the brick ('blue', 'red', 'green', etc.)
+ */
+function LegoBrick(x, y, color) {
+
+  // TODO: Write your code here.
+}
+
+// TODO: Write your code here.
+
+/**
+ * Exports.
+ *
+ * @type {LegoBrick}
+ */
+exports.LegoBrick = LegoBrick;
