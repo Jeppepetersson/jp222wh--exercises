776b54bccfb9527c482b5ec0fcc5c9214585df2b
jp222wh <jeppssonman@gmail.com>
2016-10-03 18:40:48 +0200

    Squashed 'epic-ellipse/' content from commit 11e34db
    git-subtree-dir: epic-ellipse
    git-subtree-split: 11e34db68cd5982c379700fde0960c707183836d

---
+++ src/Ellipse.js
@@ -0,0 +1,16 @@
+/**
+ * Module for Ellipse.
+ *
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+// TODO: Write your code here.
+
+/**
+ * Exports
+ */
+module.exports = Ellipse;
+
