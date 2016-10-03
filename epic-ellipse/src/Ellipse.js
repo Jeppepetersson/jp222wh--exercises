9688553027a8b4b5ff536635f140797a851b3b09
jp222wh <jeppssonman@gmail.com>
2016-10-03 19:57:31 +0200

    finished with epic ellipse, understand prototype more now, still unsure about toString.

--- epic-ellipse/src/Ellipse.js
+++ epic-ellipse/src/Ellipse.js
@@ -7,7 +7,30 @@
 
 'use strict';
 
+function Ellipse(a, b) {
+  this.a = a;
+  this.b = b;
+}
+
+Ellipse.prototype.area = function(){
+  return Math.PI * this.a * this.b;
+};
+
+Ellipse.prototype.circumference = function() {
+  return Math.PI * Math.sqrt(2 * this.a * this.a + 2 * this.b * this.b)
+};
+
+Ellipse.prototype.toString = function(){
+  return '{ a: ' + this.a +
+  ', b: ' + this.b +
+  ', area: ' + this.area().toFixed(1) +
+  ', circumference: ' + this.circumference().toFixed(1) +
+    ' }';
+};
 // TODO: Write your code here.
+
+//calculate aria Math.PI * a * b.
+//calculate circumference Math.PI * Math.sqrt(2 * a* a + 2 * b * b)
 
 /**
  * Exports
