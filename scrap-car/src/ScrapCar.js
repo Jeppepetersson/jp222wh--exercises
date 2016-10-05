eee37873a502bf1a67b9a4ff17155f5cef18b1a2
jp222wh <jeppssonman@gmail.com>
2016-10-05 14:18:26 +0200

    finnished with scrap-car

--- scrap-car/src/ScrapCar.js
+++ scrap-car/src/ScrapCar.js
@@ -9,6 +9,47 @@
 
 // TODO: Write your code here!
 
+function ScrapCar(petName = 'Skrotbilen') {
+
+  this.petName = petName;
+  this.speed = 0;
+}
+
+ScrapCar.prototype.accelerate = function(deltaSpeed) {
+
+  if (typeof deltaSpeed !== 'number' || deltaSpeed === 0) {
+    return this.petName + 's hastighet är oförändrad!';
+  }
+
+  const oldSpeed = this.speed;
+  this.speed += deltaSpeed;
+
+  return this.petName + 's hastighet ' +
+    ((deltaSpeed < 0) ? 'minskar' : 'ökar') +
+    ' från ' + oldSpeed + ' km/h till ' + this.speed + ' km/h.';
+};
+
+
+ScrapCar.prototype.turnLeft = function() {
+  return this.petName + ' svänger vänster.'
+}
+
+ScrapCar.prototype.turnRight = function() {
+  return this.petName + ' svänger höger.'
+}
+
+ScrapCar.prototype.start = function() {
+    return this.petName + ' startar.';
+  };
+
+
+ScrapCar.prototype.stop = function() {
+  this.speed = 0;
+  return this.petName + ' stannar.'
+  };
+
+
+
 /**
  *  Exports.
  */
