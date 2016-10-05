985e6e21a4cbbe78838692d89399f5b164a631f2
jp222wh <jeppssonman@gmail.com>
2016-10-05 14:18:41 +0200

    Squashed 'monumental-ellipse/' content from commit 9d6ae22
    git-subtree-dir: monumental-ellipse
    git-subtree-split: 9d6ae22e874f919d2eb08dc4c54c8ae8f19dca11

---
+++ src/Ellipse.js
@@ -0,0 +1,15 @@
+/**
+ * Module for Ellipse.
+ *
+ * @author Mats Loock
+ * @version 1.0.1
+ */
+
+'use strict';
+
+// TODO: Write your code here!
+
+/**
+ *  Exports.
+ */
+module.exports = Ellipse;
